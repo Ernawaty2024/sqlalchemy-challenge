8467328b29b7dfd2e73a9e2ef04c71e6fc760db0
Ernawaty2024 <165958285+Ernawaty2024@users.noreply.github.com>
2024-07-06 22:52:52 -0400

    update

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,22 +1,29 @@
 # Import the dependencies.
-
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 
 #################################################
 # Database Setup
 #################################################
-
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(engine, reflect = True)
 
 # Save references to each table
-
+Station = Base.classes.station
+Measurement= Base.classes.measurement
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
