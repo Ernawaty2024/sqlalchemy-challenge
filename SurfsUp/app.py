7a04b20a53aaca258a0c30cf3407f2c3305287ed
Ernawaty2024 <165958285+Ernawaty2024@users.noreply.github.com>
2024-07-09 15:48:59 -0400

    python updated

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -4,6 +4,8 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import pandas as pd
+import datetime as dt
 
 
 #################################################
@@ -28,10 +30,46 @@ session = Session(engine)
 #################################################
 # Flask Setup
 #################################################
+# Import Flask
+from flask import Flask, jsonify
 
-
-
+# Create an app
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+# 1. Home Route
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Climate API!<br/>"
+        f"Available Routes:</br>"
+        f"/api/v1.0/precipitation</br>"
+        f"/api/v1.0/stations</br>"
+        f"/api/v1.0/stations</br>"
+        f"/api/v1.0/<start></br>"
+        f"/api/v1.0/<start>/<end></br>"
+    )
+
+# 2. Precipitation Route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    
+    # Calculate date 1 year ago from the last data point in the database
+    most_recent_date = session.query(func.max(Measurement.date)).scalar()
+    most_recent_date = dt.datetime.strptime(most_recent_date, '%Y-%m-%d')
+    one_year_ago = most_recent_date - dt.timedelta(days=365)
+    
+    # Query precipitation data for the last 1 year
+    precipitation_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= one_year_ago).all()
+    
+    # Convert the query results to a dictionary
+    precipitation_dict = {date: prcp for date, prcp in precipitation_data}
+    
+    return jsonify(precipitation_dict)
+    
+               
+if __name__ == '__main__':
+    app.run(debug=True)
