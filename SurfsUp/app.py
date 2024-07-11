00875daac62588d00551a6b741ed6e4e92032cfd
Ernawaty2024 <165958285+Ernawaty2024@users.noreply.github.com>
2024-07-11 01:24:24 -0400

    update app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -38,8 +38,9 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-# 1. Home Route
+# API STATIC ROUTES
 
+# 1. Home Route
 @app.route("/")
 def welcome():
     return (
@@ -68,6 +69,61 @@ def precipitation():
     precipitation_dict = {date: prcp for date, prcp in precipitation_data}
     
     return jsonify(precipitation_dict)
+
+# 3. Stations Route
+@app.route("/api/v1.0/stations")
+def stations():
+  
+    # Query all stations
+    stations_result = session.query(Station.station).all()
+
+    # Convert list of tuples into normal list
+    stations_list= [station[0] for station in stations_result]
+
+    return jsonify(stations_list)
          
+# 4. Tobs route
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Calculate the date 1 year ago from the last data point in the database
+    most_recent_date = session.query(func.max(Measurement.date)).scalar()
+    most_recent_date = dt.datetime.strptime(most_recent_date, '%Y-%m-%d')
+    one_year_ago = most_recent_date - dt.timedelta(days=365)
+    
+    # Query most active stations
+    most_active_station = session.query(Measurement.station).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).first()[0]
+
+    # Query temperature observations for the last year from the most active station
+    tobs_data = session.query(Measurement.date, Measurement.tobs).filter(
+        Measurement.station == most_active_station,
+        Measurement.date >= one_year_ago).all()
+    
+    # Convert the query results to a list of dictionaries
+    tobs_list = [{"date": date, "tobs":tobs} for date, tobs in tobs_data]
+    
+    return jsonify(tobs_list)
+
+# API DYNAMIC ROUTE
+
+# 1. Start Route
+@app.route("/api/v1.0/<start>")
+def start(start):
+
+    # Query min, avg, max temperatures from the start date to the end of the dataset
+    Temperature_stats = session.query(
+        func.min(Measurement.tobs).label('min_temp'),
+        func.avg(Measurement.tobs).label('avg_temp'),
+        func.max(Measurement.tobs).label('max_temp'),
+    ).filter(Measurement.date == start).all()
+
+    # Convert the query results to a dictionary
+    Temperature_stats_dict = {
+        "TMIN": Temperature_stats[0][0],
+        "TAVG": Temperature_stats[0][1],
+        "TMAX": Temperature_stats[0][2] 
+    }
+
+    return jsonify(Temperature_stats_dict)
+
 if __name__ == '__main__':
     app.run(debug=True)
